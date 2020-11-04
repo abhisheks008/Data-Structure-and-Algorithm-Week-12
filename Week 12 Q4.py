906fdabf1ae52c13825ad81c42765264ee0786b1
Abhishek Sharma <sharma.abhishek1282000@gmail.com>
2020-11-04 10:15:29 +0530

    Create Week 12 Q4.py

---
+++ Week 12 Q4.py
@@ -0,0 +1,13 @@
+l = []
+i = int(input()) #enter the query
+for a in range (0,i):
+    a = input()
+    if (a[0]=='1'):
+        l.append(a[-1])
+    elif (a[0]=='2'):
+        l.insert(0,a[-1])
+    elif (a[0]=='5'):
+        for b in range (0,i-1):
+            print (l[b], end = " ")
+            if (b<3):
+                print ("->", end = " ")
